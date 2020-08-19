c8a4c1631837871599c9c3fa5e224a9222762e3d
Caio Machado de Souza Gome <caiomachadodesouza1@gmail.com>
2020-08-19 00:31:40 +0000

    Adicionado novas funções.

--- main.cs
+++ main.cs
@@ -1,11 +1,31 @@
 using System;
 
 class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-    Console.WriteLine ("Hello World");
-    Console.WriteLine ("Hello World");
-    Console.WriteLine ("Hello World");
-    Console.WriteLine ("Hello World");
+
+  //Função Print
+  public static void Exibestr () {
+    Console.WriteLine ("Teste 1");
+  }
+
+  //Função print Soma
+  public static int SomaNum () {
+    Console.WriteLine (1 + 1);
+    return 0;
+  }
+
+  //Função Input
+  public static void ExibeNum (int num) {
+    Console.WriteLine ("O número infornado foi {0}: ", num);
+  }
+
+  //Main
+  public static void Main () {
+    Exibestr ();
+
+    SomaNum ();
+
+    int num;
+    num = int.Parse(Console.ReadLine());
+    ExibeNum (num);
   }
 }
