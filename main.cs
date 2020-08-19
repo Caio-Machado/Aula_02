8d795772576657b8b78ea9f89d11ffff95fe8ca2
Caio Machado de Souza Gome <caiomachadodesouza1@gmail.com>
2020-08-19 17:52:31 +0000

    Adicionado Exercícios 01 á 04.

--- main.cs
+++ main.cs
@@ -2,30 +2,72 @@ using System;
 
 class MainClass {
 
-  //Função Print
+  //Função Print / Exercício 01
   public static void Exibestr () {
-    Console.WriteLine ("Teste 1");
+    Console.WriteLine ("Hello World");
   }
 
-  //Função print Soma
+  //Função print Soma / Teste
   public static int SomaNum () {
     Console.WriteLine (1 + 1);
     return 0;
   }
 
-  //Função Input
+  //Função Input / Exercício 02
   public static void ExibeNum (int num) {
     Console.WriteLine ("O número infornado foi {0}: ", num);
   }
 
+  //Função Soma números / Exercício 03
+  public static void SomaNum (int num1, int num2) {
+    Console.WriteLine ("A soma dos números infornados é {0}", num1 + num2);
+  }
+
+  //Função Média
+  public static void Media (int nota1, int nota2, int nota3, int nota4) {
+    Console.WriteLine ("A Média das notas informadas é {0}", (nota1 + nota2 + nota3 + nota4) / 4);
+  }
+
   //Main
   public static void Main () {
+    //EX 01
     Exibestr ();
 
+    // Teste
     SomaNum ();
 
+    //ExibeNum EX 02
     int num;
-    num = int.Parse(Console.ReadLine());
+    Console.WriteLine ("Digite um número!");
+    num = int.Parse (Console.ReadLine ());
+
     ExibeNum (num);
+
+    //SomaNum EX 03
+    int num1, num2;
+    Console.WriteLine ("Digite o primeiro número!");
+    num1 = int.Parse (Console.ReadLine ());
+
+    Console.WriteLine ("Digie o segundo número!");
+    num2 = int.Parse (Console.ReadLine ());
+
+    SomaNum (num1, num2);
+
+    //Media EX 04
+    int nota1, nota2, nota3, nota4;
+    Console.WriteLine ("Digite a primeira nota!");
+    nota1 = int.Parse (Console.ReadLine ());
+
+    Console.WriteLine ("Digite a segunda nota!");
+    nota2 = int.Parse (Console.ReadLine ());
+
+    Console.WriteLine ("Digíte a terceira nota!");
+    nota3 = int.Parse (Console.ReadLine ());
+
+    Console.WriteLine ("Digite a quarta nota!");
+    nota4 = int.Parse (Console.ReadLine ());
+
+    Media (nota1, nota2, nota3, nota4);
+
   }
 }
